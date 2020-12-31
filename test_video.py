708c6fe205e335670888f96faa7a5d9a1a15001d
burnpiro <kemalpiro@gmail.com>
2020-12-31 15:38:07 +0100

    print bboxes on video

--- test_video.py
+++ test_video.py
@@ -16,3 +16,44 @@ model = Model(DefaultDetectionModel(), DefaultSiameseModel(), Tracker(7))
 paths = model.predict_video(sys.argv[1])
 
 print(paths)
+
+cap = cv2.VideoCapture(sys.argv[1])
+
+i = 0
+frame_start = 0
+num_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+pbar = tqdm(total=num_of_frames)
+
+width = 2688
+height = 1520
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter("out.mp4", fourcc, 5, (2688, 1520))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if i > num_of_frames:
+        break
+    if frame is None:
+        break
+
+    frame = frame.astype("uint8")
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    i += 1
+    pbar.update(i)
+    for idx, track_obj in paths.items():
+        if i > len(track_obj):
+            continue
+        ymin, xmin, ymax, xmax = track_obj[i - 1]
+        rgb_frame = cv2.rectangle(
+            rgb_frame,
+            (int(xmin * width), int(ymin * height)),
+            (int(xmax * width), int(ymax * height)),
+            (0, 255, 0),
+            2,
+        )
+
+    cv2.imshow("frame", rgb_frame)
+    out.write(rgb_frame)
+
+out.release()
+cv2.destroyAllWindows()
