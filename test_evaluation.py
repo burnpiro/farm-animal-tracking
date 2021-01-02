c26c9e3907c9497e2ae657f311cd6ec2326b4d83
burnpiro <kemalpiro@gmail.com>
2021-01-02 21:15:35 +0100

    Dump eval results for given model

--- test_evaluation.py
+++ test_evaluation.py
@@ -1,5 +1,9 @@
 import sys
 import cv2
+import os
+import datetime
+import json
+import codecs
 import numpy as np
 from tqdm import tqdm
 
@@ -39,6 +43,42 @@ scores, annotations, paths = evaluator.run_evaluation_for_video(
     "test.mp4", "data/tracking/01/pigs_tracking.json", "tracking_only", 75
 )
 print(scores)
+scores = {
+    key: {"abs_err": value, "mae": value / len(paths[key])}
+    for key, value in scores.items()
+}
+
+out_dir = os.path.join(
+    "experiments", datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+)
+
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
 
 for obj_id, annotation in annotations.items():
-    cv2.imwrite(f"{obj_id}_compare.jpg", Evaluator.draw_paths_comparison(annotation[75:len(paths[obj_id])], paths[obj_id]))
+    cv2.imwrite(
+        os.path.join(out_dir, f"{obj_id}_compare.jpg"),
+        Evaluator.draw_paths_comparison(
+            annotation[75 : len(paths[obj_id])], paths[obj_id]
+        ),
+    )
+
+json.dump(
+    annotations,
+    codecs.open(os.path.join(out_dir, "annotations.json"), "w", encoding="utf-8"),
+    sort_keys=False,
+    separators=(",", ":"),
+)
+json.dump(
+    paths,
+    codecs.open(os.path.join(out_dir, "out.json"), "w", encoding="utf-8"),
+    sort_keys=False,
+    separators=(",", ":"),
+)
+json.dump(
+    scores,
+    codecs.open(os.path.join(out_dir, "scores.json"), "w", encoding="utf-8"),
+    indent=2,
+    sort_keys=False,
+    separators=(",", ":"),
+)
