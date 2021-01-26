33181b69bf09b1f95cb128e4a1c9a5cfc24db588
burnpiro <kemalpiro@gmail.com>
2021-01-26 21:39:50 +0100

    summarize scores

--- calculate_interval_score.py
+++ calculate_interval_score.py
@@ -1,43 +1,67 @@
 import os
 import json
 import codecs
+from pathlib import Path
 import numpy as np
 from data.names import names
 
+# experiments/tracking/MobileNetV2/AvgEmbeddingTracker/11_nursery_high_activity_day-cropped.mp4_2021-01-24_13-26-02/scores.json
+results_total = np.empty((17,3), dtype="U5")
+results_interval = np.empty((17,3), dtype="U5")
+# print(results)
 
-path = os.path.join('experiments/2021-01-20_10-14-03', "scores.json")
-print(path)
-scores = json.load(
-    codecs.open(path)
-)
+i = 0
+for path in Path('experiments/tracking').rglob('15_nur*/scores.json'):
+    print(path)
+    scores = json.load(
+        codecs.open(path)
+    )
+    # continue
 
-mapped_scores = []
-full_values = []
-for class_id, class_score in scores.items():
-    mean_err = np.mean(class_score["intervals"]["parts"])
-    print(class_id)
-    print(names[int(class_id)-1], mean_err)
-    mapped_scores.append((names[int(class_id)-1], mean_err))
-    full_values.append((names[int(class_id)-1], class_score["total"]["avg_err"]))
-    scores[class_id]["intervals"]["avg_err"] = mean_err
+    mapped_scores = []
+    full_values = []
+    for class_id, class_score in scores.items():
+        mean_err = np.mean(class_score["intervals"]["parts"])
+        print(class_id)
+        print(names[int(class_id)-1], mean_err)
+        mapped_scores.append((names[int(class_id)-1], mean_err))
+        full_values.append((names[int(class_id)-1], class_score["total"]["avg_err"]))
+        scores[class_id]["intervals"]["avg_err"] = mean_err
 
-print()
+    print()
 
-sorted_by = sorted(mapped_scores, key=lambda tup: tup[1])
-for class_id, class_score in sorted_by:
-    print(class_id, "{:.2f}".format(class_score*100))
+    sorted_by = sorted(mapped_scores, key=lambda tup: tup[0])
+    j = 0
+    avg_total = 0.0
+    for class_id, class_score in sorted_by:
+        results_total[j][i] = "{:.2f}".format(class_score)
+        avg_total += class_score
+        j += 1
+        # print(class_id, "{:.2f}".format(class_score))
 
-print()
+    results_total[16][i] = "{:.2f}".format(avg_total/16)
+    print()
 
-sorted_by = sorted(full_values, key=lambda tup: tup[1])
-for class_id, class_score in sorted_by:
-    print(class_id, "{:.2f}".format(class_score*100))
+    sorted_by = sorted(full_values, key=lambda tup: tup[0])
+    j = 0
+    avg_total = 0.0
+    for class_id, class_score in sorted_by:
+        results_interval[j][i] = "{:.2f}".format(class_score)
+        avg_total += class_score
+        j += 1
+        print(class_id, "{:.2f}".format(class_score))
+    results_interval[16][i] = "{:.2f}".format(avg_total/16)
 
 
-json.dump(
-    scores,
-    codecs.open(path, "w", encoding="utf-8"),
-    indent=2,
-    sort_keys=False,
-    separators=(",", ":"),
-)
+    json.dump(
+        scores,
+        codecs.open(path, "w", encoding="utf-8"),
+        indent=2,
+        sort_keys=False,
+        separators=(",", ":"),
+    )
+    i += 1
+
+print(results_total)
+
+print(results_interval)
