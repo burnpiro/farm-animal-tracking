04bcd83664bfd8263d548958e9b14a5741864b23
burnpiro <kemalpiro@gmail.com>
2021-01-30 23:57:33 +0100

    fig generating video

--- test_video.py
+++ test_video.py
@@ -1,105 +1,78 @@
 import sys
 import cv2
+import os
+import datetime
+import json
+import codecs
 import numpy as np
 from tqdm import tqdm
+import tensorflow as tf
+gpu = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpu[0], True)
+
+from model.siamese.model_generator import base_models
+from model.detection_model.detection_model import DefaultDetectionModel
+from model.siamese.siamese_model import DefaultSiameseModel
+from model.siamese.classification_model import ClassificationModel
+from model.tracker.default_tracker import DefaultTracker
+from model.tracker.simple_siamese_tracker import SimpleSiameseTracker
+from model.tracker.tracker import Tracker
+from model.tracker.avg_embedding_tracker import AvgEmbeddingTracker
+from model.tracker.default_tracker_with_path_correction import (
+    DefaultTrackerWithPathCorrection,
+)
+from model.model import Model
+from data.evaluator import Evaluator
+from data.names import names
+from helpers.score_processing import extract_scores, print_path_comparison
 
 if len(sys.argv) != 2:
     print(f"USAGE: {sys.argv[0]} <path_to_video>")
     exit()
 
-from model.detection_model.detection_model import DefaultDetectionModel
-from model.siamese.siamese_model import DefaultSiameseModel
-from model.tracker.default_tracker import DefaultTracker
-from model.tracker.simple_siamese_tracker import SimpleSiameseTracker
-from model.model import Model
 
-names = [
-    'James',
-    'Robert',
-    'William',
-    'Bob',
-    'Charles',
-    'Anthony',
-    'Paul',
-    'Steven',
-    'Kevin',
-    'George',
-    'Brian',
-    'Edward',
-    'Gary',
-    'Eric',
-    'Larry',
-    'Scott',
-    'Frank'
+dirname = os.path.dirname(__file__)
+weights_dir = os.path.join(
+    dirname, "model/siamese/weights", "MobileNetV2", "siam-118-0.0001-1.0a_0.0633.h5"
+)
+vectors_dir = os.path.join(
+    dirname, "model/siamese/vectors", "vecs-conc-MobileNetV2.tsv"
+)
+meta_dir = os.path.join(
+    dirname, "model/siamese/vectors", "meta-conc-MobileNetV2.tsv"
+)
+base_model = list(base_models.keys())[0]  # MobileNetV2
+
+# model = Model(DefaultDetectionModel(), DefaultSiameseModel(), DefaultTracker(names))
+# model = Model(DefaultDetectionModel(), DefaultSiameseModel(), Tracker(7))
+
+
+videos_paths = [
+    sys.argv[1],
 ]
-model = Model(DefaultDetectionModel(), DefaultSiameseModel(), DefaultTracker(names))
+num_of_pigs_per_video = [16]
 
-paths = model.predict_video(sys.argv[1], out_path="test_track_default.mp4")
+detection_obj = DefaultDetectionModel()
+siamese_obj = DefaultSiameseModel(weights_path=weights_dir, base_model=base_model)
+trackers = [
+    "AvgEmbeddingTracker"
+]
 
-# print(paths)
-#
-# cap = cv2.VideoCapture(sys.argv[1])
-#
-# i = 0
-# frame_start = 0
-# num_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-# pbar = tqdm(total=num_of_frames)
-#
-# width = 2688
-# height = 1520
-# fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-# out = cv2.VideoWriter("out.mp4", fourcc, 5, (2688, 1520))
-#
-# while cap.isOpened():
-#     ret, frame = cap.read()
-#     if i > num_of_frames:
-#         break
-#     if frame is None:
-#         break
-#
-#     frame = frame.astype("uint8")
-#     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#     i += 1
-#     pbar.update(i)
-#     for idx, track_obj in paths.items():
-#         if i > len(track_obj):
-#             continue
-#         ymin, xmin, ymax, xmax = track_obj[i - 1]
-#         rgb_frame = cv2.rectangle(
-#             rgb_frame,
-#             (int(xmin * width), int(ymin * height)),
-#             (int(xmax * width), int(ymax * height)),
-#             (0, 255, 0),
-#             2,
-#         )
-#
-#     cv2.imshow("frame", rgb_frame)
-#     out.write(rgb_frame)
-#
-# out.release()
-# cv2.destroyAllWindows()
+for idx in range(0, len(videos_paths)):
+    for tracker in trackers:
+        selectedTracker = None
+        if tracker == "DefaultTracker":
+            selectedTracker = DefaultTracker(names)
+        if tracker == "AvgEmbeddingTracker":
+            selectedTracker = AvgEmbeddingTracker(names, vectors_path=vectors_dir, meta_path=meta_dir, max_jump=0.2)
+        if tracker == "KalmanTracker":
+            selectedTracker = Tracker(num_of_pigs_per_video[idx])
+        video_path = videos_paths[idx]
 
-# distance_to_tracks = np.array([[1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356],
-#  [1.41421356,1.41421356,1.41421356,1.41421356,1.41421356,1.41421356
-# , 1.41421356]])
-#
-# while np.min(distance_to_tracks) != np.inf:
-#   print(np.min(distance_to_tracks))
-#   smallest_distance_pos = np.unravel_index(
-#     distance_to_tracks.argmin(), distance_to_tracks.shape
-#   )
-#   print(f"update {smallest_distance_pos[1]} with {smallest_distance_pos[0]}")
-#   print(distance_to_tracks)
-#   distance_to_tracks[smallest_distance_pos[0], :] = np.inf
-#   distance_to_tracks[:, smallest_distance_pos[1]] = np.inf
+        model = Model(
+            detection_obj,
+            siamese_obj,
+            selectedTracker,
+        )
+
+        model.predict_video(video_path, out_path="out_"+tracker+".mp4")
